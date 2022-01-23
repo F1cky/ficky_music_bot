0de4bf258a229687903150473660826374ca3371
F1cky <97504239+F1cky@users.noreply.github.com>
2022-01-23 17:20:25 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -39,7 +39,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [GitHub](https://github.com/F1cky/ficky_music_bot/) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/F1cky/)`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
